cb7bda3a758df8475c33e2ea2f6f448cdc03eec4
frankyso <frankyso@users.noreply.github.com>
2021-06-27 04:01:57 +0000

    Fix styling

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,8 @@
 namespace Konnco\ImageCast\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Orchestra\Testbench\TestCase as Orchestra;
 use Konnco\ImageCast\ImageCastServiceProvider;
+use Orchestra\Testbench\TestCase as Orchestra;
 
 class TestCase extends Orchestra
 {
